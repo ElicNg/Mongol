70179e3457e9c5c36485994760b994a0ab66ed45
Elic Ng <elicng@gmail.com>
2016-06-13 02:09:53 -0400

    Use package online

--- package.js
+++ package.js
@@ -4,21 +4,21 @@ Package.describe({
   version: '2.0.0',
   git:     'https://github.com/msavin/Mongol.git',
   documentation: 'README.md',
-  debugOnly: true
+  debugOnly: false
 });
 
 Package.onUse(function(api) {
-  
+
   var serverFiles = [
     "server/methods.js",
     "server/utilities.js"
   ];
-  
+
   api.versionsFrom("1.0");
 
   // This must go before: api.use('dburles:mongo-collection-instances@0.3.1');
-  // Weak dependency: only used if app contains package 
-  api.use('aldeed:collection2@2.3.2', {weak: true});   
+  // Weak dependency: only used if app contains package
+  api.use('aldeed:collection2@2.3.2', {weak: true});
 
   api.use('dburles:mongo-collection-instances@0.3.4');
   api.use(["underscore","check"], "server")
